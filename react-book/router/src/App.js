55579e2fbbbd80e2a17336ee92ea090d608e0940
kuzhi23 <9size.marvel@gmail.com>
2021-05-24 20:04:57 +0900

    react Router study

---
+++ react-book/router/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+import About from "./Components/about";
+import Home from "./Components/home";
+import ProFiles from "./Components/proFiles";
+import HistorySample from "./Components/historySample";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={["/about", "/info"]} component={About} />
+        <Route path="/profiles" component={ProFiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
